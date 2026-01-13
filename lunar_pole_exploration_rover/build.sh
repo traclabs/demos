f109811225d7f47693547a9b7cf39a4a330eae82
Ana C. Huaman Quispe <ana@traclabs.com>
2026-01-13 16:08:25 -0600

    Robot loads fine in gazebo, minus the plugins

--- lunar_pole_exploration_rover/build.sh
+++ lunar_pole_exploration_rover/build.sh
@@ -1,28 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-ORG=openrobotics
-IMAGE=lunar_pole_exploration_rover_demo
-TAG=latest
+set -e
 
-VCS_REF=""
-VERSION=preview
-
-# Exit script with failure if build fails
-set -eo pipefail
-
-echo ""
-echo "##### Building Space ROS Demo Docker Image #####"
-echo ""
-
-# get the folder of the script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DOKCERDIR="$DIR/Dockerfile"
-
-docker build -t $ORG/$IMAGE:$TAG \
-    --build-arg VCS_REF="$VCS_REF" \
-    --build-arg VERSION="$VERSION" $DIR \
-    -f $DOKCERDIR
-
-echo ""
-echo "##### Done! #####"
-
+docker compose build
